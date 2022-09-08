ccdb128ec567287d28070efe7ede9d19cbcd139a
MalluCortez <maria.santana@outlook.com.br>
2022-09-08 00:07:08 -0300

    feito 1 a 5

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
  
 const buttonColor = document.querySelector("#button-random-color");
- let colors = document.querySelectorAll(".color")
+ let colors = document.querySelectorAll(".variable")
 
 
  function createPalette (){
-   
+   const colorsPalette = [];
 
-    for (let index = 1; index < colors.length; index++){
+    for (let index = 0; index < colors.length; index++){
 
         let r = Math.floor(Math.random()*255);
         let g = Math.floor(Math.random()*255);
         let b = Math.floor(Math.random()*255);
 
-        colors[index].style.backgroundColor = "rgb("+r+","+g+","+b+")";
+        let boxColor = "rgb("+r+","+g+","+b+")"
+
+        colors[index].style.backgroundColor = boxColor;
+
+        colorsPalette.push(boxColor)
     }
+
+    localStorage.setItem('colorsPalette', JSON.stringify(colorsPalette));
+
 }
   
- buttonColor.addEventListener("click", createPalette)
+    buttonColor.addEventListener("click", createPalette)
+
+  function saveColors () {
+    let colorsPalette = localStorage.getItem("colorsPalette");
+
+    for (i = 0; i < colorsPalette.length; i++) {
+
+        colors[i].style.backgroundColor = JSON.parse(colorsPalette)[i];
+
+    }
+}
+
+saveColors()   
